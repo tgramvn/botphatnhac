37b2c74fa67feaa057ad24c14afd0aa5a3caaef6
tgramvn <109977670+tgramvn@users.noreply.github.com>
2022-08-31 02:09:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from config import call_py, bot
 
 
 async def main():
-    print("STARTING RIOUBOT CLIENT")
+    print("STARTING TGRAM CLIENT")
     await bot.start()
     print("STARTING PYTGCALLS CLIENT")
     await call_py.start()
     print(
         """
     ------------------------
-   | Alone Music Actived! |
+   | Tgram Music Actived! |
     ------------------------
 """
     )
